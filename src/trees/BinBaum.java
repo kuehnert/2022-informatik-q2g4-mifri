2cb04c6542db753ea964661c15db72fd34eb80a4
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-08-12 13:24:44 +0200

    Erste Stunde

--- src/trees/BinBaum.java
+++ src/trees/BinBaum.java
@@ -1,7 +1,28 @@
 package trees;
 
 public class BinBaum {
+    Knoten<Integer> wurzel;
+
+    public BinBaum() {
+        wurzel = new Knoten<Integer>(13);
+        System.out.println(wurzel);
+    }
+
+    public static void main(String[] args) {
+        new BinBaum();
+    }
 }
 
 class Knoten<T> {
+    T data;
+    Knoten<T> links;
+    Knoten<T> rechts;
+
+    public Knoten(T data) {
+        this.data = data;
+    }
+
+    public String toString() {
+        return data.toString();
+    }
 }
