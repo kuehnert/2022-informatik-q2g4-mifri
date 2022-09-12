ad674bd24abdb189a715f91a5f5ffaa6a8f90d60
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-09-12 14:01:09 +0200

    Stunde vom 12.09.2022

--- src/trees/Expertensystem.java
+++ src/trees/Expertensystem.java
@@ -1,5 +1,6 @@
 package trees;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Expertensystem {
@@ -7,29 +8,50 @@ public class Expertensystem {
     Frage wurzel;
 
     public Expertensystem() {
-        Frage fBeine = new Frage("Läuft es auf vier Beinen?", new Frage("Löwe"
-        ), new Frage("Känguru"));
-        Frage fInsekt = new Frage("Ist es ein Insekt?", new Frage("Fliege"),
+        Frage fBeine = new Frage("Läuft es auf vier Beinen",
+                new Frage("Löwe"), new Frage("Känguru"));
+        Frage fInsekt = new Frage("Ist es ein Insekt", new Frage("Fliege"),
                 new Frage("Papagei"));
-        wurzel = new Frage("Ist es ein Saeugetier?", fBeine, fInsekt);
+        wurzel = new Frage("Ist es ein Saeugetier", fBeine, fInsekt);
         eingabe = new Scanner(System.in);
     }
 
     public void spiele() {
+        System.out.println("Willkommen beim selbstlernenden Expertensystem");
+        boolean weiter = true;
+
+        while (weiter) {
+            spieleRunde();
+            weiter = jaNeinFrage("Noch eine Runde");
+        }
+    }
+
+    // 1. Robins Idee
+    // lrl|Ist es ein Saegetier?
+    // lrll|Affe
+    // lrlr|Borkenkaefer
+
+    // 2. Marius' Idee
+    // Speichere als NLR (einfach), lese ein (schwierig)
+
+    // 3. Max' Idee
+    // Verwende Java-Serialisierung
+
+    // 4. Besser nicht JSON
+
+    public void spieleRunde() {
         // Schleife "willst Du nochmal spielen?"
         Frage runner = wurzel;
-        Frage pred = null;
 
         while (true) {
             if (!runner.istBlatt()) {
-                System.out.println(runner.inhalt + " (j/n): ");
-                boolean antwortJa = eingabe.nextLine().startsWith("j");
-                pred = runner;
-                runner = antwortJa ? runner.ja : runner.nein;
+                runner = jaNeinFrage(runner.inhalt) ? runner.ja : runner.nein;
             } else {
-                System.out.println("Ist es etwa ein(e) " + runner.inhalt +
-                        "?" + " (j/n):");
-                boolean antwortJa = eingabe.nextLine().startsWith("j");
+                boolean antwortJa =
+                        jaNeinFrage("Ist es etwa ein(e) " + runner.inhalt);
+
+                String altesTier = runner.inhalt;
+
                 if (antwortJa) {
                     System.out.println("Hurra, ich habe Dein Tier geraten");
                     // nochmal spielen?
@@ -39,14 +61,11 @@ public class Expertensystem {
                     System.out.println("Oh je, ich kenne Dein Tier nicht.");
                     System.out.println("An welches Tier hast Du gedacht? ");
                     String neuesTierStr = eingabe.nextLine();
-                    System.out.println("Gib eine Frage, wie sich altesTier " +
-                            "und neuesTier von einander unterscheiden, und " +
-                            "für die für neuesTier die Antwort 'ja' ist? ");
+                    System.out.println("Gib eine Frage, wie sich " + altesTier + " " + "und " + neuesTierStr + " von einander unterscheiden, und" + "für die für " + neuesTierStr + " die Antwort 'ja' ist? ");
                     String neueFrageStr = eingabe.nextLine();
-                    Frage neuesTier = new Frage(neuesTierStr);
-                    Frage neueFrage = new Frage(neueFrageStr, neuesTier,
-                            runner);
-                    pred.ja = neueFrage;
+                    runner.ja = new Frage(neuesTierStr);
+                    runner.nein = new Frage(altesTier);
+                    runner.inhalt = neueFrageStr;
                     return;
                 }
             }
@@ -55,6 +74,11 @@ public class Expertensystem {
 
     public static void main(String[] args) {
         new Expertensystem().spiele();
+    }
+
+    public boolean jaNeinFrage(String frage) {
+        System.out.print(frage + " (j/n)? ");
+        return eingabe.nextLine().toLowerCase(Locale.ROOT).startsWith("j");
     }
 }
 
