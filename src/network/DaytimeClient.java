ad0b89fa33c5e2ac24ea03a7ca2902e6e5439bc2
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2023-01-12 09:25:40 +0100

    Stunde vom 12.01.2023

---
+++ src/network/DaytimeClient.java
@@ -0,0 +1,28 @@
+package network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class DaytimeClient {
+    // InputStream inputStream = sock.getInputStream();
+    // InputStreamReader isreader = new InputStreamReader(inputStream);
+    // BufferedReader reader = new BufferedReader(isreader);
+
+    public static void main(String[] args) {
+        try {
+            Socket sock = new Socket("10.2.129.148", 10013);
+            Scanner scanner = new Scanner(sock.getInputStream());
+
+            String input = scanner.nextLine();
+            System.out.println("Empfangen: " + input);
+
+            sock.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
